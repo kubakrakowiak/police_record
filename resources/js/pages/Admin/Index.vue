247003c73525cfc7ff14721dfee176acbc183131
kubakrakowiak <jakubkrakowiak00@gmail.com>
2022-05-11 22:39:42 +0200

    fixed grade edit, added tariff list in ACP

--- resources/js/pages/Admin/Index.vue
+++ resources/js/pages/Admin/Index.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <div>
+        <div>
+            <h2 class="h2 justify-center flex content-center" style="font-family: 'Roboto'">ACP Software developed by Craco</h2>
+        </div>
+        <div>
+            <h3 class="h3 justify-center flex content-center" style="font-family: 'Roboto'">v0.6.0-alpha Administration Control Panel </h3>
+        </div>
+    </div>
 </template>
 
 <script>
